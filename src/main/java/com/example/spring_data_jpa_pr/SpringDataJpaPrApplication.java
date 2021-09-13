b434b69ff439c3efc7aaab3668c0cc5959555172
Your Name <you@example.com>
2021-09-13 23:14:33 +0700

    crud_native

--- src/main/java/com/example/spring_data_jpa_pr/SpringDataJpaPrApplication.java
+++ src/main/java/com/example/spring_data_jpa_pr/SpringDataJpaPrApplication.java
@@ -1,34 +1,16 @@
 package com.example.spring_data_jpa_pr;
 
-import com.example.spring_data_jpa_pr.entity.Customer;
-import com.example.spring_data_jpa_pr.repository.CustomerRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
 
 @SpringBootApplication
 public class SpringDataJpaPrApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SpringDataJpaPrApplication.class, args);
     }
 
-    @Bean
-    public CommandLineRunner run(CustomerRepository customerRepository){
-        return args -> {
-            insertEmployee(customerRepository);
-        };
-    }
-    private void insertEmployee(CustomerRepository customerRepository){
-        Customer customer = Customer.builder()
-                .name("long")
-                .email("s2taaa@gmail.com")
-                .age(21)
-                .build();
-        customerRepository.save(customer);
 
-    }
 
 }
--- src/main/java/com/example/spring_data_jpa_pr/repository/CustomerRepository.java
+++ src/main/java/com/example/spring_data_jpa_pr/repository/CustomerRepository.java
@@ -1,9 +1,35 @@
 package com.example.spring_data_jpa_pr.repository;
 
 import com.example.spring_data_jpa_pr.entity.Customer;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+import java.util.List;
+
 @Repository
 public interface CustomerRepository extends CrudRepository<Customer,Long> {
+
+    @Query(value = "select * from tbl_customer", nativeQuery = true)
+    List<Customer> getAll();
+
+    @Query(value = "select * from tbl_customer where name = 'long'", nativeQuery = true)
+    List<Customer> getListByName(String name);
+
+    @Transactional
+    @Modifying
+    @Query(value = "insert into tbl_customer values(1,21,'longk','s2kaaa')", nativeQuery = true)
+    void addCustomer(String name);
+
+    @Transactional
+    @Modifying
+    @Query(value = "update tbl_customer set name = ?'longcute' where id = ?2", nativeQuery = true)
+    void updateCustomer(Integer id, String name);
+
+    @Transactional
+    @Modifying
+    @Query(value = "delete from person where id = ?1", nativeQuery = true)
+    void deleteCustomerById(Integer id);
 }
